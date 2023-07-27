0b42e5e190424af13205ba0d968e24d235532343
am-User 175 <am-User175@edu.bfi.wien>
2023-07-27 15:22:53 +0200

    started to add functionality to contact form

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+let contactForm = document.getElementById('contactForm');
+let contactName = document.getElementById('contactName');
+let contactEmail = document.getElementById('contactEmail');
+let contactSubject = document.getElementById('contactSubject');
+let contactMessage= document.getElementById('contactMessage');
+let submitContactForm = document.getElementById('submitContactForm');
+
+submitContactForm.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+});
